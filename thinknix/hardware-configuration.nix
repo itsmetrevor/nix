713ca352510f25e46e3884ffdb4a8bcb9dc53750
itsmetrevor <huffsmith07@gmail.com>
2025-05-22 21:30:55 -0400

    thinknix hardware config added

---
+++ thinknix/hardware-configuration.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  # Enable TRIM for SSD
+  services.fstrim.enable = true;
+  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ]
+
+  fileSystems."/" = { 
+    device = "/dev/disk/by-uuid/98dc7f64-623f-4835-8fef-e4413b851da5";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = { 
+    device = "/dev/disk/by-uuid/A898-7B83";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
+  swapDevices = [ 
+    { device = "/dev/disk/by-uuid/c1bf48f5-03ae-4b71-84ad-e6a3f00a7abe"; }
+  ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
